232247287daae0d1d7f01829e74cdf38d4a2a714
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2019-11-02 01:51:26 -0300

    Criação de uma classe vetor com metodos para manipular os dados.

--- EstruturaDados-Algoritmos-Java/src/estruturadados/vetor/Vetor.java
+++ EstruturaDados-Algoritmos-Java/src/estruturadados/vetor/Vetor.java
@@ -1,9 +1,87 @@
 package estruturadados.vetor;
 
 public class Vetor {
+
 	private String[] elementos;
-	
+	private int tamanho;
+
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
 	}
+	
+	public int getTamanho() {
+		return this.tamanho;
+	}
+
+	private void aumentaCapacidade() {
+		if(this.tamanho == this.elementos.length) {
+			String[] elementosNovos = new String[this.elementos.length * 2];
+			for(int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+	public void adiciona(String elemento) {
+		this.aumentaCapacidade();
+		this.elementos[this.tamanho] = elemento;
+		this.tamanho++;
+	}
+	
+	public void adiciona(String elemento, int posicao) {
+		this.aumentaCapacidade();
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		for(int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i+1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+	}
+	
+	public void remove(int posicao) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		for(int i = posicao; i < this.tamanho - 1; i++) {
+			this.elementos[i] = this.elementos[i+1];
+		}
+		this.tamanho--;
+		
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		for (int i = 0; i < this.tamanho -1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		if(this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho -1]);	
+		}
+		s.append("]");
+		return s.toString();
+	}
+	
+	public String buscaElemento(int posicao) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		return this.elementos[posicao];
+	}
+	
+	public int buscaElemento(String elemento) {
+		for(int i = 0; i < this.tamanho; i++) {
+			if(this.elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
 }
+
