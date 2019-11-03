cd57d575be4d9b43dee3d1fb473b75d23247b818
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2019-11-03 02:27:59 -0200

    Desenvolvendo uma classe vetor generico para todos tipos de dados.

---
+++ EstruturaDados-Algoritmos-Java/src/estruturadados/vetorgenerico/VetorGenerico.java
@@ -0,0 +1,84 @@
+package estruturadados.vetorgenerico;
+
+public class VetorGenerico<T> {
+	private T[] elementos;
+	private int tamanho;
+
+	public VetorGenerico(int capacidade) {
+		this.elementos = (T[])new Object[capacidade];
+		this.tamanho = 0;
+	}
+	
+	public int getTamanho() {
+		return this.tamanho;
+	}
+
+	private void aumentaCapacidade() {
+		if(this.tamanho == this.elementos.length) {
+			T[] elementosNovos = (T[]) new Object [this.elementos.length * 2];
+			for(int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+	public void adiciona(T elemento) {
+		this.aumentaCapacidade();
+		this.elementos[this.tamanho] = elemento;
+		this.tamanho++;
+	}
+	
+	public void adiciona(T elemento, int posicao) {
+		this.aumentaCapacidade();
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		for(int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i+1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+	}
+	
+	public void remove(int posicao) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		for(int i = posicao; i < this.tamanho - 1; i++) {
+			this.elementos[i] = this.elementos[i+1];
+		}
+		this.tamanho--;
+		
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		for (int i = 0; i < this.tamanho -1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		if(this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho -1]);	
+		}
+		s.append("]");
+		return s.toString();
+	}
+	
+	public Object buscaElemento(int posicao) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		return this.elementos[posicao];
+	}
+	
+	public int buscaElemento(T elemento) {
+		for(int i = 0; i < this.tamanho; i++) {
+			if(this.elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
