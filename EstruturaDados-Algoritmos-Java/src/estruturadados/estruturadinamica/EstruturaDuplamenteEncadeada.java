b51749e69be0475e4142fb009de8deb9967516f5
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-01-27 23:49:07 -0200

    Criação do metodo adiciona por posição na estrutura duplamente encadeada

--- EstruturaDados-Algoritmos-Java/src/estruturadados/estruturadinamica/EstruturaDuplamenteEncadeada.java
+++ EstruturaDados-Algoritmos-Java/src/estruturadados/estruturadinamica/EstruturaDuplamenteEncadeada.java
@@ -19,12 +19,71 @@ public class EstruturaDuplamenteEncadeada <T>{
 			novoNo.setProximo(novoNo);
 			this.tamanho++;
 		}else {
-			this.ultimo.setProximo(novoNo);
-			novoNo.setAnterior(this.ultimo);
-			this.ultimo = novoNo;
 			novoNo.setProximo(inicio);
+			novoNo.setAnterior(this.ultimo);
+			this.ultimo.setProximo(novoNo);
+			this.ultimo = novoNo;
+			
 			this.tamanho++;
 		}
+	}
+	
+	//protected 
+	public void adiciona(T elemento, int posicao) {
+		if(posicao < 0 || posicao > tamanho) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		NoDuplamenteEncadeado novoNo = new NoDuplamenteEncadeado(elemento);
+		if(posicao == 0) {
+			novoNo.setAnterior(this.inicio.getAnterior());
+			novoNo.setProximo(this.inicio);
+			this.inicio.setAnterior(novoNo);
+			this.inicio = novoNo;
+			this.tamanho++;			
+		}else {
+			NoDuplamenteEncadeado aux = buscarNo(posicao+1);
+			aux.setAnterior(novoNo);
+			novoNo.setProximo(aux);
+			aux = buscarNo(posicao-1);
+			aux.setProximo(novoNo);
+			novoNo.setAnterior(aux);
+			this.tamanho++;
+		}
+		
+	}
+	
+	protected NoDuplamenteEncadeado buscarNo(int posicao) {
+		if(posicao < 0 || posicao > tamanho) {
+			throw new IllegalArgumentException("Posição invalida");
+		}
+		if(posicao == 0) {
+			return inicio;
+		}
+		if(posicao == tamanho) {
+			return ultimo;
+		}
+		NoDuplamenteEncadeado aux = this.inicio;
+		for(int i = 0; i < posicao; i++) {
+			aux = aux.getProximo();
+		}
+		return aux;
+	}
+	
+	public String toString() {
+		if(estaVazia()) {
+			return "[]";
+		}
+		NoDuplamenteEncadeado aux = this.inicio;
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		for (int i = 0; i < this.tamanho-1; i++) {
+			s.append(aux.getElemento());
+			s.append(", ");
+			aux = aux.getProximo();
+		}
+		s.append(aux.getElemento());
+		s.append("]");
+		return s.toString();
 	}
 	
 	public boolean estaVazia() {
