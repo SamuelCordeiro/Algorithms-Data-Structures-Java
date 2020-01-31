0327f5de6d71537383ae8efd89aa507d7df2611b
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-01-31 01:07:57 -0200

    BubbleSort finalizado

--- EstruturaDados-Algoritmos-Java/src/estruturadados/algoritimosordenacao/BubbleSort.java
+++ EstruturaDados-Algoritmos-Java/src/estruturadados/algoritimosordenacao/BubbleSort.java
@@ -3,15 +3,14 @@ package estruturadados.algoritimosordenacao;
 public class BubbleSort {
 	public static <T extends Comparable<T>> void sort(T[] vetor) {
 		T aux;
-		for (int i = vetor.length; i > 0; i--) {
-			for(int j = 0; j < vetor.length; j++) {
+		for (int i = vetor.length -1; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
 				if(vetor[j].compareTo(vetor[j+1]) > 0) {
 					aux = vetor[j];
 					vetor[j] = vetor[j+1];
 					vetor[j+1] = aux;
 				}
 			}
-			
 		}
 	}
 }
